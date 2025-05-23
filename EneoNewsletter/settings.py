e0aa7fea8f7ca0300c3e69f6cfbea57a61190bd9
Landry Simo <landrysimo99@gmail.com>
2025-05-23 15:12:20 +0100

    Update database configuration to use environment variables
    Replaced hardcoded SQLite configuration with dynamic settings using `dj_database_url` and `DATABASE_URL` environment variable. This change improves flexibility and prepares the project for different deployment environments. Unused database configurations have been commented out for clarity.

--- EneoNewsletter/settings.py
+++ EneoNewsletter/settings.py
@@ -97,13 +97,20 @@ WSGI_APPLICATION = 'EneoNewsletter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
